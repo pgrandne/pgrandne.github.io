7ce7026a0b163047559fb87d47be6772e74e4bad
Perrin GRANDNE <perrin.grandne@gmail.com>
2025-10-13 15:33:12 +0200

    Add static export

---
+++ out/_next/static/R5VIFWCpBpUxrA5gUY-iO/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/2ac59d5bccd5c853.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ out/_next/static/chunks/a583eb1ce166c236.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/1fc4b8a20ac551b4.js",
+  "static/chunks/9839ff6d6fa61358.js",
+  "static/chunks/turbopack-a09f6142669bf91a.js"
+])
