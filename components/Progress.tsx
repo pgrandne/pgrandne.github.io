e008c0f6952816075aa07e903eb949ab4160736b
Perrin <perrin.grandne@gmail.com>
2026-01-12 18:44:52 +0100

    Animate gradient

--- components/Progress.tsx
+++ components/Progress.tsx
@@ -1,7 +1,12 @@
 import { EncryptedText } from './ui/encrypted-text'
 
 export const Progress = ({ textProgress }: { textProgress: string }) => (
-  <p className="mx-auto max-w-lg py-10 text-left">
-    <EncryptedText text={textProgress} encryptedClassName="text-neutral-500" revealedClassName="dark:text-white text-black" revealDelayMs={100} />
+  <p className="text-base text-center">
+    <EncryptedText
+      text={textProgress}
+      encryptedClassName="text-neutral-500"
+      revealedClassName="dark:text-white text-black font-alpha-lyrae"
+      revealDelayMs={100}
+    />
   </p>
 )
