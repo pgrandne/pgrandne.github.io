397311789c485226f60fab67650aec1ea3764a54
Perrin <perrin.grandne@gmail.com>
2026-03-24 08:48:49 +0100

    Add Umami

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Script from 'next/script'
 import { ThemeProvider } from '@/components/ui/theme-provider'
 import './globals.css'
 
@@ -16,6 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
+      <head>
+        <Script
+          src="https://cloud.umami.is/script.js"
+          data-website-id="3f0eeb51-6f4e-450a-97a4-2c28501a44f7"
+          strategy="afterInteractive"
+          data-domains="pgrandne.github.io,perrin.website"
+        />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} ${alphaLyra.variable} antialiased`}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
           {children}
